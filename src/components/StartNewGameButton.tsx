72bc54018ebcd77914cf15d36327a95ffbc645de
nbogie <neillbogie@googlemail.com>
2024-03-03 22:52:46 +0000

    use variants to keep buttons consistent

--- src/components/StartNewGameButton.tsx
+++ src/components/StartNewGameButton.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
-
+import { clamp } from "lodash";
 export function StartNewGameButton({
     onClick,
 }: {
@@ -8,43 +8,50 @@ export function StartNewGameButton({
 }) {
     const [numLetters, setNumLetters] = useState(8);
 
-    function inc() {
-        setNumLetters((p) => constrain(p + 1, 3, 9));
-    }
     function dec() {
-        setNumLetters((p) => constrain(p - 1, 3, 9));
+        setNumLetters((p) => clamp(p - 1, 3, 9));
     }
+
+    function inc() {
+        setNumLetters((p) => clamp(p + 1, 3, 9));
+    }
+
+    const variants = {
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+        shrinking: { scale: 0.8 },
+    };
+
     return (
         <div>
             <motion.button
+                variants={variants}
                 className="gameSelectButton"
                 onClick={() => onClick(numLetters)}
-                whileTap={{ scale: 0.9 }}
+                whileTap={"shrinking"}
             >
                 New Game
             </motion.button>
             with
             <motion.button
+                variants={variants}
                 className="gameSelectButton"
                 onClick={dec}
-                whileTap={{ scale: 0.9 }}
+                whileTap={"shrinking"}
             >
                 {" "}
                 -{" "}
             </motion.button>
             <span>{numLetters} letters</span>
             <motion.button
+                variants={variants}
                 className="gameSelectButton"
                 onClick={inc}
-                whileTap={{ scale: 0.9 }}
+                whileTap={"shrinking"}
             >
                 {" "}
                 +{" "}
             </motion.button>
         </div>
     );
-}
-
-function constrain(inputNum: number, min: number, max: number) {
-    return Math.min(max, Math.max(min, inputNum));
 }
