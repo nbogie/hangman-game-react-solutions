67972448b0d21baca9084e89bd0b185d76e817eb
nbogie <neillbogie@googlemail.com>
2024-03-03 23:34:25 +0000

    b0rked letters display - won't stagger anim

--- src/components/LettersDisplay.tsx
+++ src/components/LettersDisplay.tsx
@@ -1,8 +1,64 @@
+import { motion } from "framer-motion";
+
 export function LettersDisplay({
     lettersOrNulls,
 }: {
     lettersOrNulls: (null | string)[];
 }) {
     const lettersOrSpaces = lettersOrNulls.map((l) => (l === null ? "_" : l));
-    return <div className="letters">{lettersOrSpaces.join(" ")}</div>;
+    const animVariantsForContainer = {
+        smaller: {
+            scale: 0.8,
+            transition: {
+                staggerChildren: 0.3, // Delay between each child animation
+            },
+        },
+        normal: {
+            scale: 1,
+            transition: {
+                staggerChildren: 0.3, // Delay between each child animation
+            },
+        },
+    };
+
+    const itemVariants = {
+        smaller: { scale: 0.8 },
+        normal: { scale: 1 },
+    };
+
+    return (
+        <motion.div
+            className="lettersOrSpaces"
+            variants={animVariantsForContainer}
+            initial={"smaller"}
+            animate={"normal"}
+        >
+            {lettersOrSpaces.map((l, ix) => (
+                <LetterOrSpace
+                    key={l + "_at_" + ix}
+                    variants={itemVariants}
+                    letterOrSpace={l}
+                />
+            ))}
+        </motion.div>
+    );
+}
+
+function LetterOrSpace({
+    letterOrSpace,
+    variants,
+}: {
+    variants: Record<string, object>;
+    letterOrSpace: string | null;
+}) {
+    return (
+        <motion.div
+            variants={variants}
+            initial={"smaller"}
+            animate={"normal"}
+            className="letterOrSpace"
+        >
+            {letterOrSpace}
+        </motion.div>
+    );
 }
